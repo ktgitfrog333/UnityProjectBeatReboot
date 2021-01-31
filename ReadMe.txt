4b1356723f7bb6023856df29c02f05d4a66405cd
ktgitfrog333 <paraysis+git@gmail.com>
2021-01-31 21:22:56 +0900

    2021/01/31 21:20 ★StandardAsetとユニティちゃん追加と操作ロジック作成 ・StandardAsetとユニティちゃんをコミット対象から除外 .gitignore ・操作処理の追加 ProjectSettings\ProjectSettings.asset UserSettings\EditorUserSettings.asset ・操作アニメーションを追加 Assets\Animations\PlayerAnimatorController.controller ・メインゲーム画面にて、キャラクター配置など Assets\Scenes\02_Stage001.unity ・新規スクリプトファイル作成時にUTF-8（BOM）変換 Assets\Editor\AssetPostprocessUTF8Encode.cs ・キャラクター操作制御スクリプトクラスを作成 Assets\Scripts\ScCharacterController.cs ・定数クラスを作成 Assets\Scripts\Commons\CsNormalLevelDesignOfCommon.cs

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,3 +1,21 @@
+2021/01/31 21:20
+★StandardAsetとユニティちゃん追加と操作ロジック作成
+・StandardAsetとユニティちゃんをコミット対象から除外
+.gitignore
+・操作処理の追加
+ProjectSettings\ProjectSettings.asset
+UserSettings\EditorUserSettings.asset
+・操作アニメーションを追加
+Assets\Animations\PlayerAnimatorController.controller
+・メインゲーム画面にて、キャラクター配置など
+Assets\Scenes\02_Stage001.unity
+・新規スクリプトファイル作成時にUTF-8（BOM）変換
+Assets\Editor\AssetPostprocessUTF8Encode.cs
+・キャラクター操作制御スクリプトクラスを作成
+Assets\Scripts\ScCharacterController.cs
+・定数クラスを作成
+Assets\Scripts\Commons\CsNormalLevelDesignOfCommon.cs
+
 2021/01/31 12:31
 ★新規プロジェクト作成
 ・コミット対象設定ファイルの追加
---
+++ UnityProjectBeatReboot/UnityProjectBeatReboot/Assets/Scripts/Commons/CsNormalLevelDesignOfCommon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 定数・ロジック管理
+/// </summary>
+namespace Commons
+{
+    /// <summary>
+    /// 定数管理クラス
+    /// </summary>
+    public static class CsNormalLevelDesignOfCommon
+    {
+        /// <summary>
+        /// 移動（左スティック）のHorizontal
+        /// </summary>
+        public static readonly string LEFT_HORIZONTAL = "Horizontal";
+        /// <summary>
+        /// 移動（左スティック）のHorizontal
+        /// </summary>
+        public static readonly string LEFT_VERTICAL = "Vertical";
+    }
+}
