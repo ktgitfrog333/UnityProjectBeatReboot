7be2e7f6a722e89fe5c22c779818a44615eff75b
ktgitfrog333 <paraysis+git@gmail.com>
2021-03-20 21:22:09 +0900

    ★演奏トリガーに触れると演奏開始カメラ演出 ・ゲームステージ画面にて、演出カメラ設置など Assets\Scenes\02_Stage001.unity ・プレイヤー自動移動ロジック追加と操作ロジックの一部修正 Assets\Scripts\ScCharacterController.cs ・トリガー処理後に演奏開始カットシーンの起動とFTP視点へ切り替え Assets\Scripts\ScPlayedTrigger.cs ・演奏開始アニメーション Assets\Animations\StartPlaying_Cut1.anim Assets\Animations\StartPlaying_Cut1.controller Assets\Animations\StartPlaying_Cut2.anim Assets\Animations\StartPlaying_Cut2.controller Assets\Animations\StartPlaying_Cut3.anim Assets\Animations\StartPlaying_Cut3.controller Assets\Animations\StartPlaying_Cut4.anim Assets\Animations\StartPlaying_Cut4.controller ・トリガーのプレハブを作成 Assets\Prefabs\PlayedTrigger.prefab ・演奏開始カットシーンを作成 Assets\Timelines\StartPlayingDirect.playable ProjectSettings\TimelineSettings.asset

--- UnityProjectBeatReboot/UnityProjectBeatReboot/Assets/Scripts/ScPlayedTrigger.cs
+++ UnityProjectBeatReboot/UnityProjectBeatReboot/Assets/Scripts/ScPlayedTrigger.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 /// <summary>
 /// プレイヤーが触れた際に何かしらの処理を実行する
@@ -11,12 +12,66 @@ public class ScPlayedTrigger : MonoBehaviour
     [SerializeField] private ScFPSCController _controller;
     /// <summary>プレイヤー</summary>
     [SerializeField] private GameObject _player;
+    /// <summary>プレイヤーカメラ</summary>
+    [SerializeField] private GameObject _playerMainCamera;
+    /// <summary>演奏開始アニメーション再生フラグ</summary>
+    private bool _startPlayFlag = true;
+    /// <summary>演奏開始カメラ</summary>
+    [SerializeField] private GameObject _startPlayCamera;
+    /// <summary>演奏開始カットシーン</summary>
+    [SerializeField] private GameObject _startPlayCut;
+
+    [SerializeField] private GameObject _targetObject;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name.Equals(_player.name))
         {
+            if (_startPlayFlag == true)
+            {
+                StartCoroutine(_player.GetComponent<ScCharacterController>().CharacterWalkInPlace(this.gameObject));
+            }
+        }
+    }
+
+    /// <summary>
+    /// プレイヤーの動きを止めて演出映像を動かす
+    /// </summary>
+    public void StopPlayerAndStartProductionMovie()
+    {
+        _player.GetComponent<ScCharacterController>()._playerControllerFlag = false;
+        _playerMainCamera.SetActive(false);
+        _startPlayCamera.SetActive(true);
+        _startPlayCut.SetActive(true);
+        _startPlayFlag = false;
+        if (_startPlayCut.activeSelf == true)
+        {
+            var playable = _startPlayCut.GetComponent<PlayableDirector>();
+            StartCoroutine(RuningStatusForCinematicCamera(playable));
+        }
+    }
+
+    /// <summary>
+    /// 映像開始アニメーションのタイムラインを監視<param/>
+    /// 停止したら映像カメラを無効にしてプレイヤーのメインカメラを有効にする
+    /// </summary>
+    /// <param name="playable">演奏開始カットシーン</param>
+    /// <returns></returns>
+    private IEnumerator RuningStatusForCinematicCamera(PlayableDirector playable)
+    {
+        yield return null;
+        if (playable.state.ToString().Equals("Playing"))
+        {
+            StartCoroutine(RuningStatusForCinematicCamera(playable));
+        }
+        else if (playable.state.ToString().Equals("Paused"))
+        {
             _controller.EnableFPS();
+            _startPlayCamera.SetActive(false);
+            _startPlayCut.SetActive(false);
+            _startPlayFlag = true;
+            _player.GetComponent<ScCharacterController>()._playerControllerFlag = true;
+            StopCoroutine(RuningStatusForCinematicCamera(playable));
         }
     }
 
@@ -25,6 +80,7 @@ public class ScPlayedTrigger : MonoBehaviour
         if (other.gameObject.name.Equals(_player.name))
         {
             _controller.DisableFPS();
+            _playerMainCamera.SetActive(true);
         }
     }
 }
